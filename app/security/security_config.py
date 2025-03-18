8f1678eb71e89cb90cf737fe56924f6171f96516
Mohan Singh <mohan.singh.upr@gmail.com>
2025-03-18 20:20:57 +0530

    handle error message from clerk

--- app/security/security_config.py
+++ app/security/security_config.py
@@ -4,14 +4,13 @@ from starlette import status
 
 
 async def authenticated_user(request: Request):
-
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        from app.main import settings # to prevent circular import
+        from app.main import settings  # to prevent circular import
         request_state = authenticate_request(
             request,
             AuthenticateRequestOptions(
@@ -19,11 +18,15 @@ async def authenticated_user(request: Request):
                 authorized_parties=settings.clerk_authorized_parties,
             ),
         )
-        if request_state.reason:
-            raise credentials_exception
+        if not request_state.is_signed_in:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=request_state.message,
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         username: str = request_state.payload.get("sub")
         if username is None:
             raise credentials_exception
-    except Exception:
-        raise credentials_exception
+    except Exception as e:
+        raise e
     return username
